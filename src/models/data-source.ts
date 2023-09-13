5e3bd4552b645b0addce7fce60322e59b1f4c3cf
Hyboy95 <temilator@gmail.com>
2023-09-13 12:02:08 +0700

    fix

--- src/models/data-source.ts
+++ src/models/data-source.ts
@@ -3,12 +3,12 @@ import { DataSource } from "typeorm"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "mydb.cnpntblhte9k.ap-southeast-1.rds.amazonaws.com",
+    host: "database-1.cnpntblhte9k.ap-southeast-1.rds.amazonaws.com",
     port: 3306,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB,
-    synchronize: false,
+    synchronize: true,
     logging: false,
     entities: ["dist/src/models/entity/*.js"],
     // migrations: ["dist/src/migrations/*.js"],
